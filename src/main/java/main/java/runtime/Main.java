ab1efe7beb83c712013b306462ab0e3d557964b1
xephon2 <s.eike85@gmail.com>
2014-01-03 16:41:12 +0100

    Optimized code styling settings. Configured new clone of project in a new VM.

---
+++ src/main/java/main/java/runtime/Main.java
@@ -0,0 +1,112 @@
+package main.java.runtime;
+
+import java.awt.EventQueue;
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import main.java.club.Athlete;
+import main.java.club.Club;
+import main.java.club.ClubMember;
+import main.java.gui.Application;
+import main.java.gui.Login;
+import main.java.observer.XMLManager;
+
+import org.xml.sax.SAXException;
+
+/**
+ * The Main class starts the application.
+ * 
+ * @author Stefan Eike (s.eike85@gmail.com)
+ */
+public class Main {
+
+	/** Stores an instance of the RuntimeManager. */
+	private static RuntimeManager runtimeManager;
+
+	/** Stores the thread sleeptime. */
+	private static final int SLEEP_TIME = 500;
+
+	/**
+	 * Start the application.
+	 * 
+	 * @param args
+	 *            Input parameters
+	 * @throws ParserConfigurationException
+	 *             TBD
+	 * @throws SAXException
+	 *             TBD
+	 * @throws IOException
+	 *             TBD
+	 */
+	public static void main(final String[] args)
+			throws ParserConfigurationException, SAXException, IOException {
+
+		final Club club = new Club("myclub");
+		runtimeManager = new RuntimeManager(club);
+
+		final XMLManager xman = new XMLManager(runtimeManager, "clubmembers");
+		// TableManager tman = new TableManager();
+
+		/*
+		 * FIXME This does not work. Either create a runtime manager WITH a club
+		 * or load a club from XML WITHOUT having a RuntimeManager.
+		 */
+		// Club club = xman.loadXMLClub();
+		// runtimeManager.setClub(club);
+
+		if (xman != null) {
+			System.out.println("XMLManager established.");
+			club.registerClubObserver(xman);
+			xman.update();
+		}
+
+		// if (tman != null) {
+		// System.out.println("Tablemanager established.");
+		// club.registerClubObserver(tman);
+		// tman.update();
+		// }
+
+		final ClubMember dummy = new Athlete(333);
+		dummy.setUsername("dummy");
+		dummy.setFirstName("dummy");
+		dummy.setLastName("dummy");
+		dummy.hashAndStorePassword("dummy");
+		club.addClubMember(dummy);
+
+		/**
+		 * Launch the application.
+		 */
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					new Login(runtimeManager).getFrame().setVisible(true);
+				} catch (final Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+		// Ask every 500ms if someone is logged in.
+		while (runtimeManager.getLoggedInUser() == null) {
+			try {
+				Thread.sleep(SLEEP_TIME);
+			} catch (final InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					final Application app = new Application(runtimeManager);
+					app.getFrame().setVisible(true);
+				} catch (final Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
